b4e07b62926f4ccac5064ff470594b047beb18a8
rhinoed <36555602+rhinoed@users.noreply.github.com>
2025-02-05 01:21:05 -0800

    change string::size_type to unsigned int

--- reversed_strings/reversedStrings.cpp
+++ reversed_strings/reversedStrings.cpp
@@ -21,7 +21,7 @@ int main() {
     // get length of string
     string::size_type const length = userString.size();
     // loop over string in reverse
-    for ( string::size_type i = length; i > 0; i--) {
+    for ( unsigned int i = length; i > 0; i--) {
         // append to string
         reversedString.append(1, userString[i - 1]);
     }

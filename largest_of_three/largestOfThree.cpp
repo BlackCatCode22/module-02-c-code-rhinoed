d3a4569ab5a97a3c33a1d46af6e7336fa7646c30
rhinoed <36555602+rhinoed@users.noreply.github.com>
2025-02-06 22:36:25 -0800

    refactor add recursive largestOfThree

--- largest_of_three/largestOfThree.cpp
+++ largest_of_three/largestOfThree.cpp
@@ -16,7 +16,8 @@ int getAnIntFromTheUser(){
     cin >> userInput;
     return userInput;
 }
-// while loop implementation
+
+// // while loop implementation
 int largestOrThree(const int num ) {
     int largest = 0;
     int i = 1;
@@ -43,10 +44,27 @@ int largestOfThree() {
     return largest;
 }
 
-int main() {
+// largestOfThree recursive
+ int largestOfThree(const int i, const int num) {
+    // get user input
+    const int num1 = getAnIntFromTheUser();
+    // set largest
+    const int largest = (num1 > num) ? num1 : num;
+    // check for base case i = 3
+    return (i < 3) ? largestOfThree(i + 1, largest) : largest;
+}
 
-    const int largest = largestOfThree();
-    cout << "The largest of the three is: " + to_string(largest) << endl;
+int main() {
+    // create initial num value
+    constexpr int num = INT_MIN;
+    // output largest of three ints
+    // there are three different functions with the name largestOfThree.
+    // They each demonstrate different ways I thought of to solve this problem
+    // To call the 'while loop' version call with largestOfThree(getIntFromTheUser())
+    // The 'if else' version call with no arguments
+    // The recursive version call with two arguments
+    cout << "The largest of the three is: " + to_string(largestOfThree(1,num)) << endl;
+
 
     return 0;
 }
